5d7fc96e1b7e2491dfa054c61f3938171eeedfec
Santhosh <vsaisanthosh02@gmail.com>
2026-03-06 22:39:08 +0530

    UC12: Strategy Pattern for Palindrome Algorithms
    Implemented two strategies for palindrome checking using Stack and Deque.

--- UC12.java
+++ UC12.java
@@ -1,1 +1,54 @@
+import java.util.Stack;
+import java.util.Deque;
+import java.util.ArrayDeque;
 
+interface PalindromeStrategy {
+    boolean checkPalindrome(String word);
+}
+
+class StackStrategy implements PalindromeStrategy {
+    public boolean checkPalindrome(String word) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < word.length(); i++) {
+            stack.push(word.charAt(i));
+        }
+
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) != stack.pop()) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+class DequeStrategy implements PalindromeStrategy {
+    public boolean checkPalindrome(String word) {
+        Deque<Character> deque = new ArrayDeque<>();
+        for (int i = 0; i < word.length(); i++) {
+            deque.addLast(word.charAt(i));
+        }
+
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast()) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+public class UseCase12PalindromeCheckerApp {
+    public static void main(String[] args) {
+        String word = "level";
+        PalindromeStrategy strategy;
+
+        strategy = new StackStrategy();
+
+        if (strategy.checkPalindrome(word)) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is not a Palindrome");
+        }
+    }
+}
